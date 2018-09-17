5cc24dfbf428554d0d460e83e4bf534dc2079da9
Brian Compagnat <brian.compagnat@gmail.com>
2018-09-17 14:20:57 -0700

    Implementation of user connection with password

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,10 +8,35 @@ using USherbrooke.ServiceModel.Sondage;
 
 namespace Client
 {
+    class Auth
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
     class Program
     {
+
         static HttpClient httpClient = new HttpClient();
         const string API_KEY = "A2D3-HTDG-MLU2-3AM5"; // Need to generate a new one
+        static Auth auth = new Auth
+        {
+            username = "Carey",
+            password = "Price"
+        };
+
+        static async Task<int> ConnectAsync()
+        {
+            int result = 0;
+
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync(
+                "api/connect", auth);
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsAsync<int>();
+            }
+            
+            return result;
+        }
 
         static async Task<IList<Poll>> GetSondages()
         {
@@ -36,7 +61,7 @@ namespace Client
             //specify to use TLS 1.2 as default connection
             System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-
+            Console.WriteLine("Hi, tying to connect to the server...");
 
             httpClient.BaseAddress = new Uri("https://localhost:44364/");
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", API_KEY);
@@ -44,10 +69,19 @@ namespace Client
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             // Need to check connection
+            try
+            {
+               int  userID = await ConnectAsync();
+                Console.WriteLine("UserID : " + userID);
 
-            IList<Poll> sondages = await GetSondages();
-
-            // maybe validate sondages ??
+               // IList<Poll> sondages = await GetSondages();
+                // maybe validate sondages ??
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
 
             Console.WriteLine("Hello World!");
         }
